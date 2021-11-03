a5128a3b537371522402060aa9081e7fc62a8c95
vijay <vijayletchumikrishnan@gmail.com>
2021-11-04 00:01:39 +0800

    change

--- project1/list.js
+++ project1/list.js
@@ -21,9 +21,13 @@ function GetBooking() {
 
             //load all rows from Sheety API
             for (let i = 0; i < json.bookings.length; i++) {
+                let gName = json.bookings[i].from;
+                let gEmail = json.bookings[i].to;
+                let gPax = json.bookings[i].pax;
+                let gRemarks = json.bookings[i].onward date;
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
-                let gPax = json.bookings[i].pax;
+                let gPax = json.bookings[i].returndate;
                 let gRemarks = json.bookings[i].remarks;
                 let gId = json.bookings[i].id;
                 let btnId = "delete" + gId;
@@ -32,12 +36,26 @@ function GetBooking() {
                 row.insertCell(0).innerHTML = gId
                 row.insertCell(1).innerHTML = gName
                 row.insertCell(2).innerHTML = gEmail
-                row.insertCell(3).innerHTML = gPax
-                row.insertCell(4).innerHTML = gRemarks 
-                row.insertCell(5).innerHTML = "<button id='" + btnId + "' type='button' class='btn btn-danger'>Delete</button>"
+                row.insertCell(3).innerHTML = gFrom
+                row.insertCell(4).innerHTML = gTo
+                row.insertCell(5).innerHTML = gOnwardDate
+                row.insertCell(6).innerHTML = gReturnDtare
+                row.insertCell(7).innerHTML = gPax
+                row.insertCell(8).innerHTML = gRemarks 
+                row.insertCell(9).innerHTML = "<button id='" + btnId + "' type='button' class='btn btn-danger'>Delete</button>"
 
                 bookingIds.push(btnId)
             }
+            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">From</th>
+                            <th scope="col">To</th>
+                            <th scope="col">Onward Date</th>
+                            <th scope="col">Return Date</th>
+                            <th scope="col">Pax</th>
+                            <th scope="col">Remarks</th>
+                            <th scope="col">Task</th>
 
             for (let j = 0; j < bookingIds.length; j++) {
                 let el = document.getElementById(bookingIds[j])
