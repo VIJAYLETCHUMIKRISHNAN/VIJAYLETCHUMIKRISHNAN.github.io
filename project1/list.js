5fd9f9f1dc44b9992370663e444dc86592b93167
vijay <vijayletchumikrishnan@gmail.com>
2021-11-03 16:52:39 +0800

    ny

--- project1/list.js
+++ project1/list.js
@@ -4,7 +4,7 @@ refreshNowBtn.addEventListener("click", function () {
 })
 
 function GetBooking() {
-    let url = 'https://api.sheety.co/b0388b3fdb8e9911c2bc43abcb013008/bookingApp/bookings';
+    let url = 'https://api.sheety.co/aaeea4ce6d96025869ecd70915bf8d5b/untitledSpreadsheet/bookings';
     fetch(url)
         .then((response) => response.json())
         .then(json => {
@@ -23,10 +23,8 @@ function GetBooking() {
             for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
-                let gRoom = json.bookings[i].roomtype;
-                let gArrival = json.bookings[i].arrivaldate;
-                let gDeparture = json.bookings[i].departuredate;
-                let gRequest = json.bookings[i].specialRequest;
+                let gPax = json.bookings[i].pax;
+                let gRemarks = json.bookings[i].remarks;
                 let gId = json.bookings[i].id;
                 let btnId = "delete" + gId;
 
@@ -34,11 +32,9 @@ function GetBooking() {
                 row.insertCell(0).innerHTML = gId
                 row.insertCell(1).innerHTML = gName
                 row.insertCell(2).innerHTML = gEmail
-                row.insertCell(3).innerHTML = gRoom
-                row.insertCell(4).innerHTML = gArrival
-                row.insertCell(5).innerHTML = gDeparture
-                row.insertCell(6).innerHTML = gRequest 
-                row.insertCell(7).innerHTML = "<button id='" + btnId + "' type='button' class='btn btn-danger'>Delete</button>"
+                row.insertCell(3).innerHTML = gPax
+                row.insertCell(4).innerHTML = gRemarks 
+                row.insertCell(5).innerHTML = "<button id='" + btnId + "' type='button' class='btn btn-danger'>Delete</button>"
 
                 bookingIds.push(btnId)
             }
@@ -56,7 +52,7 @@ function GetBooking() {
 }
 
 function DeleteBooking(id) {
-    let url = 'https://api.sheety.co/b0388b3fdb8e9911c2bc43abcb013008/bookingApp/bookings/' + id;
+    let url = 'https://api.sheety.co/aaeea4ce6d96025869ecd70915bf8d5b/untitledSpreadsheet/bookings' + id;
     fetch(url, {
         method: 'DELETE',
     })
